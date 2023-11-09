afe7054748f669cbaceb43ebeb94c36c689a5dcd
Pam <pwillisop@yahoo.com>
2023-11-09 15:30:54 -0500

    added View Recipes

---
+++ public/js/profile.js
@@ -0,0 +1,15 @@
+const viewRecipe = async () => {
+    const response = await fetch('/api/users/profile', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+    });
+  
+    if (response.ok) {
+      document.location.replace('/');
+    } else {
+      alert(response.statusText);
+    }
+  };
+  
+  document.querySelector('#profile').addEventListener('click', viewRecipe);
+  
