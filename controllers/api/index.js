32c123aec13e258d0677812be2fefd18e8dc866b
Pam <pwillisop@yahoo.com>
2023-11-09 22:29:41 -0500

    view button

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const recipeRoutes = require('./recipeRoute');
 const usersRoutes = require('./usersRoutes');
+// const profileRoutes = require('./profileRoute');
 
 router.use('/recipe', recipeRoutes);
 router.use('/users', usersRoutes);
+// router.use('/profile', profileRoutes);
 
 module.exports = router;
