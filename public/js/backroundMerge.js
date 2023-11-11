94616b378fb0f8edfd824295a6da89e2f077057b
Michaelepigott <michaelepigott@gmail.com>
2023-11-10 20:01:01 -0500

    Search bg fixed

--- public/js/backroundMerge.js
+++ public/js/backroundMerge.js
@@ -1,6 +1,6 @@
 // JavaScript
 var multiple = new Multiple({
   selector: '.item',
-  background: "url('../css/6124bc62f54fd325b2177042_ella-olsson-oPBjWBCcAEo-unsplash.jpg')",
+  background: "url('http://localhost:3006/css/6124bc62f54fd325b2177042_ella-olsson-oPBjWBCcAEo-unsplash.jpg')",
 });
 
